a827cb9d7648edf2cf85a0a38bdde1306c40f6ef
AriaTod <144653662+AriaTod@users.noreply.github.com>
2026-06-08 04:51:18 +0300

    Add files via upload

--- gateway/server.js
+++ gateway/server.js
@@ -1,1 +1,34 @@
+const { ApolloServer } = require('@apollo/server');
+const { startStandaloneServer } = require('@apollo/server/standalone');
+const { ApolloGateway } = require('@apollo/gateway');
 
+async function startGateway() {
+    const gateway = new ApolloGateway({
+        serviceList: [
+            {
+                name: 'users',
+                url: 'http://localhost:4001'
+            },
+            {
+                name: 'products',
+                url: 'http://localhost:4002'
+            },
+            {
+                name: 'orders',
+                url: 'http://localhost:4003'
+            }
+        ]
+    });
+
+    const server = new ApolloServer({
+        gateway
+    });
+
+    const { url } = await startStandaloneServer(server, {
+        listen: { port: 4000 }
+    });
+
+    console.log(`Gateway running at ${url}`);
+}
+
+startGateway();
