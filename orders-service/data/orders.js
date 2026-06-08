b4769883e253d88f2e557d60694fec6b99bd193a
AriaTod <144653662+AriaTod@users.noreply.github.com>
2026-06-08 04:53:01 +0300

    Add files via upload

---
+++ orders-service/data/orders.js
@@ -0,0 +1,14 @@
+const orders = [
+    {
+        id: "1",
+        userId: "1",
+        productId: "1"
+    },
+    {
+        id: "2",
+        userId: "2",
+        productId: "2"
+    }
+];
+
+module.exports = orders;
--- orders-service/server.js
+++ orders-service/server.js
@@ -1,1 +1,104 @@
+const { ApolloServer } = require('@apollo/server');
+const { startStandaloneServer } = require('@apollo/server/standalone');
+const { buildSubgraphSchema } = require('@apollo/subgraph');
+const gql = require('graphql-tag');
 
+const orders = require('./data/orders');
+
+const typeDefs = gql`
+    type Order {
+        id: ID!
+        userId: ID!
+        productId: ID!
+    }
+
+    type Query {
+        orders: [Order!]!
+        order(id: ID!): Order
+    }
+
+    type Mutation {
+        createOrder(
+            userId: ID!
+            productId: ID!
+        ): Order!
+
+        updateOrder(
+            id: ID!
+            userId: ID!
+            productId: ID!
+        ): Order
+
+        deleteOrder(id: ID!): Boolean!
+    }
+`;
+
+const resolvers = {
+    Query: {
+        orders: () => orders,
+
+        order: (_, { id }) => {
+            return orders.find(order => order.id === id);
+        }
+    },
+
+    Mutation: {
+        createOrder: (_, { userId, productId }) => {
+            const newOrder = {
+                id: String(orders.length + 1),
+                userId,
+                productId
+            };
+
+            orders.push(newOrder);
+
+            return newOrder;
+        },
+
+        updateOrder: (_, { id, userId, productId }) => {
+            const order = orders.find(
+                order => order.id === id
+            );
+
+            if (!order) {
+                return null;
+            }
+
+            order.userId = userId;
+            order.productId = productId;
+
+            return order;
+        },
+
+        deleteOrder: (_, { id }) => {
+            const index = orders.findIndex(
+                order => order.id === id
+            );
+
+            if (index === -1) {
+                return false;
+            }
+
+            orders.splice(index, 1);
+
+            return true;
+        }
+    }
+};
+
+async function startServer() {
+    const server = new ApolloServer({
+        schema: buildSubgraphSchema({
+            typeDefs,
+            resolvers
+        })
+    });
+
+    const { url } = await startStandaloneServer(server, {
+        listen: { port: 4003 }
+    });
+
+    console.log(`Orders Service running at ${url}`);
+}
+
+startServer();
